c165a12e1fa8fdd54cffb311847567cc2cf8a77e
e551763 <alexander.nikonoff@sbb.ch>
2025-04-22 19:34:17 +0200

    chore: unit test fix

--- src/test/java/ch/sbb/polarion/extension/aad/synchronizer/model/MemberResponseWrapperTest.java
+++ src/test/java/ch/sbb/polarion/extension/aad/synchronizer/model/MemberResponseWrapperTest.java
@@ -11,8 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import static ch.sbb.polarion.extension.aad.synchronizer.model.MemberResponseWrapper.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MemberResponseWrapperTest {
 
@@ -30,13 +29,29 @@ class MemberResponseWrapperTest {
             assertNotNull(memberResponseWrapper);
             List<Member> members = memberResponseWrapper.getValue();
             assertEquals(5, members.size());
-            assertEquals("https://graph.microsoft.com/v1.0/users?$skip=5", memberResponseWrapper.getNextLink());
+            assertNull(memberResponseWrapper.getNextLink());
             Optional<Member> johnDoe = members.stream()
                     .filter(member -> member.getEmail().equals("john.doe@example.com"))
                     .findFirst();
             Assertions.assertTrue(johnDoe.isPresent());
             assertEquals("jdoe", johnDoe.get().getId());
             assertEquals("John Doe", johnDoe.get().getName());
+        }
+    }
+
+    @Test
+    void testParseNextLink() throws IOException {
+        Map<String, String> fieldsMapping = Map.ofEntries(
+                Map.entry(ID, "mailNickname"),
+                Map.entry(NAME, "displayName"),
+                Map.entry(EMAIL, "mail")
+        );
+
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("membersPaginated.json")) {
+            String content = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            MemberResponseWrapper memberResponseWrapper = MemberResponseWrapper.fromJsonList(content, fieldsMapping);
+            assertNotNull(memberResponseWrapper);
+            assertEquals("http://localhost:1080/v1.0/groups/next/members", memberResponseWrapper.getNextLink());
         }
     }
 }
