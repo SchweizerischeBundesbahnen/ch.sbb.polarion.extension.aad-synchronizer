7aed3f6048ed30b3fe14893b75125306e623cb0b
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2026-03-12 22:08:47 +0100

    refactor: replace MockServer with WireMock in tests
    Switch test implementation from MockServer to WireMock for simplification and better support. Adjusted related test cases and dependency configuration in the POM file.

--- src/test/java/ch/sbb/polarion/extension/aad/synchronizer/connector/GraphConnectorTest.java
+++ src/test/java/ch/sbb/polarion/extension/aad/synchronizer/connector/GraphConnectorTest.java
@@ -10,46 +10,42 @@ import ch.sbb.polarion.extension.aad.synchronizer.model.OrganizationDataWrapper;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.polarion.core.config.IOAuth2SecurityConfiguration;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockserver.integration.ClientAndServer;
-import org.mockserver.model.Header;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockserver.integration.ClientAndServer.startClientAndServer;
-import static org.mockserver.matchers.Times.exactly;
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.response;
 
 @ExtendWith(MockitoExtension.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@WireMockTest
 class GraphConnectorTest {
 
     private final IOAuth2SecurityConfiguration authenticationProviderConfiguration = new FakeOAuth2SecurityConfiguration();
-    private final GraphConnector graphConnector = new GraphConnector(authenticationProviderConfiguration,"test", "http://localhost:1080");
     private final String groupPrefix = "test";
-    protected ClientAndServer mockServer;
 
+    private GraphConnector createConnector(WireMockRuntimeInfo wmRuntimeInfo) {
+        return new GraphConnector(authenticationProviderConfiguration, "test", wmRuntimeInfo.getHttpBaseUrl());
+    }
 
     private static Stream<Arguments> testMemberValues() {
-
         return Stream.of(
                 Arguments.of("members.json", 5),
                 Arguments.of("emptyMembersList.json", 0)
@@ -57,15 +53,14 @@ class GraphConnectorTest {
     }
 
     private static Stream<Arguments> testGroupValues() {
-
         return Stream.of(
                 Arguments.of(200, NotFoundException.class, "No AAD groups were found"),
                 Arguments.of(222, InvalidGraphResponseException.class, "Could not get proper response from Microsoft Graph"),
                 Arguments.of(401, InvalidGraphResponseException.class, "Microsoft Graph token expired")
         );
     }
-    private static Stream<Arguments> testOrganizationValues() {
 
+    private static Stream<Arguments> testOrganizationValues() {
         return Stream.of(
                 Arguments.of(200, NotFoundException.class, "No Organization data was found"),
                 Arguments.of(222, InvalidGraphResponseException.class, "Could not get proper response from Microsoft Graph"),
@@ -73,64 +68,54 @@ class GraphConnectorTest {
         );
     }
 
-    @BeforeAll
-    void startServer() {
-        mockServer = startClientAndServer(1080);
-    }
-
-    @AfterAll
-    void stopServer() {
-        mockServer.stop();
-    }
-
     @ParameterizedTest
     @MethodSource("testMemberValues")
-    void getMembers(String pathToJson, Integer expectedSize) throws IOException {
+    void getMembers(String pathToJson, Integer expectedSize, WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
         String memberId = "memberId";
         mockGetMemberCall(memberId, pathToJson, 200);
 
-        List<Member> memberList = graphConnector.getMembers(memberId);
+        List<Member> memberList = createConnector(wmRuntimeInfo).getMembers(memberId);
 
         assertThat(memberList).hasSize(expectedSize);
     }
 
     @Test
-    void getPaginatedMembers() throws IOException {
+    void getPaginatedMembers(WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
         String memberId = "memberId";
-        mockGetMemberCall(memberId, "membersPaginated.json", 200);
+        String baseUrl = wmRuntimeInfo.getHttpBaseUrl();
+        mockGetMemberCallWithBody(memberId, getContent("membersPaginated.json").replace("http://localhost:1080", baseUrl), 200);
         mockGetMemberCall("next", "members.json", 200);
 
-        List<Member> memberList = graphConnector.getMembers(memberId);
+        List<Member> memberList = createConnector(wmRuntimeInfo).getMembers(memberId);
 
         assertThat(memberList).hasSize(8);
     }
 
     @Test
-    void getMembersWithWrongStatusCode() throws IOException {
+    void getMembersWithWrongStatusCode(WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
         String memberId = "memberId";
         mockGetMemberCall(memberId, "emptyMembersList.json", 222);
 
-        assertThatThrownBy(() -> graphConnector.getMembers(memberId))
+        GraphConnector connector = createConnector(wmRuntimeInfo);
+        assertThatThrownBy(() -> connector.getMembers(memberId))
                 .isInstanceOf(InvalidGraphResponseException.class)
                 .hasMessageContaining("Could not get proper response from Microsoft Graph");
     }
 
     @Test
-    void getGroups() throws IOException {
-
+    void getGroups(WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
         mockGetGroupsCall("groups.json", 200);
 
-        List<Group> groupList = graphConnector.getGroups(groupPrefix);
+        List<Group> groupList = createConnector(wmRuntimeInfo).getGroups(groupPrefix);
 
         assertThat(groupList).hasSize(5);
     }
 
     @Test
-    void getOrganizationData() throws IOException {
-
+    void getOrganizationData(WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
         mockGetOrganizationDataCall("organizationData.json", 200);
 
-        OrganizationData data = graphConnector.getOrganizationData();
+        OrganizationData data = createConnector(wmRuntimeInfo).getOrganizationData();
         ObjectMapper mapper = new ObjectMapper();
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
@@ -138,96 +123,72 @@ class GraphConnectorTest {
 
         assertThat(data).isEqualTo(wrapper.getValue().get(0));
     }
+
     @ParameterizedTest
     @MethodSource("testOrganizationValues")
-    void getEmptyOrganizationDataResponse(Integer code, Class<?> clazz, String message) throws IOException {
-
+    void getEmptyOrganizationDataResponse(Integer code, Class<?> clazz, String message, WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
         mockGetOrganizationDataCall("emptyOrganizationData.json", code);
 
-        assertThatThrownBy(graphConnector::getOrganizationData)
+        GraphConnector connector = createConnector(wmRuntimeInfo);
+        assertThatThrownBy(connector::getOrganizationData)
                 .isInstanceOf(clazz)
                 .hasMessageContaining(message);
     }
 
     @ParameterizedTest
     @MethodSource("testGroupValues")
-    void getEmptyGroupsResponse(Integer code, Class<?> clazz, String message) throws IOException {
-
+    void getEmptyGroupsResponse(Integer code, Class<?> clazz, String message, WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
         mockGetGroupsCall("emptyGroupsList.json", code);
 
-        assertThatThrownBy(() -> graphConnector.getGroups(groupPrefix))
+        GraphConnector connector = createConnector(wmRuntimeInfo);
+        assertThatThrownBy(() -> connector.getGroups(groupPrefix))
                 .isInstanceOf(clazz)
                 .hasMessageContaining(message);
     }
 
     @Test
-    void wrongJsonDataResponse() throws IOException {
-
+    void wrongJsonDataResponse(WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
         String memberId = "memberId";
 
         mockGetGroupsCall("wrongJsonData.json", 200);
         mockGetOrganizationDataCall("wrongJsonData.json", 200);
         mockGetMemberCall(memberId, "wrongJsonData.json", 200);
 
-        assertThatThrownBy(() -> graphConnector.getGroups(groupPrefix))
+        GraphConnector connector = createConnector(wmRuntimeInfo);
+        assertThatThrownBy(() -> connector.getGroups(groupPrefix))
                 .isInstanceOf(ResponseParsingException.class);
-        assertThatThrownBy(graphConnector::getOrganizationData)
+        assertThatThrownBy(connector::getOrganizationData)
                 .isInstanceOf(ResponseParsingException.class);
-        assertThatThrownBy(() -> graphConnector.getMembers(memberId))
+        assertThatThrownBy(() -> connector.getMembers(memberId))
                 .isInstanceOf(JsonParseException.class);
     }
 
     private void mockGetGroupsCall(String path, Integer statusCode) throws IOException {
-        mockServer.when(
-                        request()
-                                .withMethod("GET")
-                                .withPath("/v1.0/groups")
-                                .withQueryStringParameter("$filter", "startswith(displayName, 'test')"),
-                        exactly(1)
-                )
-                .respond(
-                        response()
-                                .withStatusCode(statusCode)
-                                .withHeaders(
-                                        new Header("Content-Type", "application/json; charset=utf-8"),
-                                        new Header("Cache-Control", "public, max-age=86400"))
-                                .withBody(getContent(path))
-                                .withDelay(TimeUnit.SECONDS, 1));
+        stubFor(get(urlPathEqualTo("/v1.0/groups"))
+                .willReturn(aResponse()
+                        .withStatus(statusCode)
+                        .withHeader("Content-Type", "application/json; charset=utf-8")
+                        .withBody(getContent(path))));
     }
 
     private void mockGetOrganizationDataCall(String path, Integer statusCode) throws IOException {
-        mockServer.when(
-                        request()
-                                .withMethod("GET")
-                                .withPath("/v1.0/organization"),
-                        exactly(1)
-                )
-                .respond(
-                        response()
-                                .withStatusCode(statusCode)
-                                .withHeaders(
-                                        new Header("Content-Type", "application/json; charset=utf-8"),
-                                        new Header("Cache-Control", "public, max-age=86400"))
-                                .withBody(getContent(path))
-                                .withDelay(TimeUnit.SECONDS, 1));
+        stubFor(get(urlPathEqualTo("/v1.0/organization"))
+                .willReturn(aResponse()
+                        .withStatus(statusCode)
+                        .withHeader("Content-Type", "application/json; charset=utf-8")
+                        .withBody(getContent(path))));
     }
 
-
     private void mockGetMemberCall(String memberId, String path, Integer statusCode) throws IOException {
-        mockServer.when(
-                        request()
-                                .withMethod("GET")
-                                .withPath("/v1.0/groups/" + memberId + "/members"),
-                        exactly(1)
-                )
-                .respond(
-                        response()
-                                .withStatusCode(statusCode)
-                                .withHeaders(
-                                        new Header("Content-Type", "application/json; charset=utf-8"),
-                                        new Header("Cache-Control", "public, max-age=86400"))
-                                .withBody(getContent(path))
-                                .withDelay(TimeUnit.SECONDS, 1));
+        mockGetMemberCallWithBody(memberId, getContent(path), statusCode);
+    }
+
+    private void mockGetMemberCallWithBody(String memberId, String body, Integer statusCode) {
+        stubFor(get(urlPathEqualTo("/v1.0/groups/" + memberId + "/members"))
+                .willReturn(aResponse()
+                        .withStatus(statusCode)
+                        .withHeader("Content-Type", "application/json; charset=utf-8")
+                        .withBody(body)));
     }
 
     private String getContent(String path) throws IOException {
